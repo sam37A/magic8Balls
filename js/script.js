ed7451af0c12360ceac326f74df7fb974bf8ccf2
sam37A <sahilsan30@gmail.com>
2022-02-17 17:25:18 -0800

    magic8 ball project first commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const imageArray=["./img/magic8ball_1.png","./img/magic8ball_2.png","./img/magic8ball_3.png","./img/magic8ball_4.png","./img/magic8ball_5.png","./img/magic8ball_6.png","./img/magic8ball_7.png","./img/magic8ball_8.png"
+
+]
+
